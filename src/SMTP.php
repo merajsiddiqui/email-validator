47be4f400e68264efbfaa8421703e27bd963b5f7
Meraj Ahmad Siddiqui <meraj.siddiqui@rankwatch.com>
2017-04-07 17:01:01 +0530

    SMTP class Modified

--- src/SMTP.php
+++ src/SMTP.php
@@ -21,7 +21,7 @@ class SMTP {
 	 * Public IP address of your current server
 	 * @var string
 	 */
-	private $host;
+	private $host = "localhost";
 
 	/**
 	 * Domain on which email is hosted
@@ -59,12 +59,11 @@ class SMTP {
 	 */
 	public $read_timeout = 10;
 
-	public function __construct() {
-		$ip_api = "http://ipinfo.io/json";
-		$content = file_get_contents($ip_api);
-		$content_decoded = json_decode($content);
-		$this->host = $content_decoded->ip;
-	}
+	/**
+	 * Set debug mode as true to debug the response
+	 * @var boolean
+	 */
+	public $debug_mode = true;
 
 	public function validate($email_id) {
 		if ($email_id) {
@@ -82,6 +81,10 @@ class SMTP {
 		$mx_priority = [];
 		if (function_exists("getmxrr")) {
 			getmxrr($this->domain, $mx_host, $mx_priority);
+			if ($this->debug_mode) {
+				print_r($mx_host);
+				print_r($mx_priority);
+			}
 		} else {
 			//do something
 		}
@@ -98,15 +101,19 @@ class SMTP {
 	protected function communicateThroughSocket() {
 		$acceptence_codes = [250, 451, 452];
 		//Starting socket connection to read
-		foreach ($this->mx_records as $mx_record) {
+		$mx_count = 0;
+		while (isset($this->mx_records[$mx_count])) {
 			$this->socket = fsockopen(
-				$mx_record['host'],
+				$this->mx_records[$mx_count]['host'],
 				$this->port,
 				$err_code,
 				$err_message,
 				(float) $this->connection_timeout
 			);
+			var_dump($err_code);
 			stream_set_timeout($this->socket, $this->read_timeout);
+			break;
+			$mx_count++;
 		}
 		//Reading response from socket
 		if ($this->socket) {
@@ -120,19 +127,34 @@ class SMTP {
 			//communicate send message to server
 			$my_address = $this->host;
 			$sender = "webmaster@$my_address";
-			fwrite($this->socket, "HELO $my_address");
-			fwrite($this->socket, "MAIL FROM: <$sender>");
-			fwrite($this->socket, "RCPT TO: <$sender>");
-			$reply = fread($this->socket, 2082);
+			$this->SendMessage("HELO $my_address");
+			$this->SendMessage("MAIL FROM: <$sender>");
+			$reply = $this->SendMessage("RCPT TO: <$sender>");
 			var_dump($reply);
 			//parse response code from reply message
 			preg_match('/^([0-9]{3})/ims', $reply, $matches);
 			$rcpt_response_code = isset($matches[1]) ? (int) $matches[1] : 0;
 			$email_valid = (in_array($rcpt_response_code, $acceptence_codes)) ? true : false;
-			fwrite($this->socket, "quit");
+			$this->SendMessage("quit");
 			fclose($this->socket);
-			var_dump($email_valid);
 			return $email_valid;
 		}
 	}
+
+	public function SendMessage($message) {
+		fwrite($this->socket, $message);
+		$reply_message = fread($this->socket, 2082);
+		if ($this->debug_mode) {
+			echo "Send: \t $message \n Rply: \t $reply_message";
+		}
+		return $reply_message;
+	}
 }
+
+//Test
+
+$email = "meraj.siddiqui@rankwatch.com";
+
+$abc = new SMTP();
+$ab = $abc->validate($email);
+var_dump($ab);
